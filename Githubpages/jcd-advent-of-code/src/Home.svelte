02a880f57ffaaf6c35a8778b67c6c9f7913ee08f
Jens Christian Dalgaard <jcd@netcompany.com>
2023-12-02 15:18:47 +0100

    more

---
+++ Githubpages/jcd-advent-of-code/src/Home.svelte
@@ -0,0 +1,48 @@
+<script>
+  
+
+	
+	 let snowflakes = [];
+ 
+	function addSnowflake() {
+    snowflakes = [
+      ...snowflakes,
+      {
+        size: Math.random() * 20 + 10,
+        left: Math.random() * 100, 
+        duration: Math.random() * 5 + 3
+      }
+    ];
+
+    if (snowflakes.length > 200) {
+      snowflakes.shift();
+    }
+  }
+  
+	setInterval(addSnowflake, 300);
+	
+	</script>
+  
+  <style>
+
+  
+
+  
+	.content {
+	  background-color: #4d4d4d; 
+	  padding: 20px;
+	  margin: 20px auto;
+	  max-width: 600px; 
+	}
+  
+
+
+  </style>
+  
+  <main>
+	<h1>Advent of Code Solutions</h1>
+	<div class="content">
+	  <p>This repo contains the different solutions to the Advent of Code.</p>
+
+	</div>
+  </main>
