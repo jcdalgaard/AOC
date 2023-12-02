29f934b42d4c074515194c2bc75fb00a971955f9
Jens Christian Dalgaard <jcd@netcompany.com>
2023-12-02 07:50:06 +0100

    Init svelte setup

---
+++ Githubpages/jcd-advent-of-code/src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Snowflake from './Snowflake.svelte';
+  
+	 let snowflakes = [];
+ 
+	function addSnowflake() {
+    snowflakes = [
+      ...snowflakes,
+      {
+        size: Math.random() * 20 + 10,
+        left: Math.random() * 100, 
+        duration: Math.random() * 5 + 3
+      }
+    ];
+
+    if (snowflakes.length > 200) {
+      snowflakes.shift();
+    }
+  }
+  
+	setInterval(addSnowflake, 300); // Add a snowflake every 100ms
+	
+	</script>
+  
+  <style>
+
+  
+	main {
+	  text-align: center; /* Center text in the main tag */
+	  height: 120vh;
+    margin: 0;
+    padding: 0;
+    background-color: #333333; /* Dark grey background */
+    color: #FFD700; /* Yellow text */
+    font-family: Arial, sans-serif;
+	}
+  
+	.content {
+	  background-color: #4d4d4d; /* Lighter dark grey background for the content */
+	  padding: 20px;
+	  margin: 20px auto;
+	  max-width: 600px; /* Adjust width as needed */
+	}
+  
+	h1 {
+	}
+  
+	/* Additional styling as needed */
+  </style>
+  
+  <main>
+	<h1>Advent of Code Solutions</h1>
+	<div class="content">
+	  <p>This repo contains the different solutions to the Advent of Code.</p>
+	  <!-- More content can be added here -->
+	</div>
+  
+  {#each snowflakes as snowflake}
+	  <Snowflake size={snowflake.size} left={snowflake.left} duration={snowflake.duration} />
+  {/each}
+  </main>
---
+++ Githubpages/jcd-advent-of-code/src/Snowflake.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let size;
+    export let left;
+    export let duration;
+    console.log(left)
+  </script>
+  
+  <style>
+    .snowflake {
+      position: fixed;
+      top: -10px; /* Start above the viewport */
+      color: white; /* Ensure contrast against background */
+      user-select: none;
+      pointer-events: none;
+      font-size: var(--size);
+      animation: fall var(--duration) linear infinite;
+    }
+  
+    @keyframes fall {
+      to {
+        transform: translateY(100vh); /* Fall to the bottom of the viewport */
+      }
+    }
+  </style>
+  
+  <div class="snowflake" style="--size: {size}px; left: {--left}%; --duration: {duration}s;">
+    ❄
+  </div>
