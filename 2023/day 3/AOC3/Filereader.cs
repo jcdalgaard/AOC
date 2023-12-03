2d2952ad4119c08aa49bc4f58f76164e7093b2e5
Jens Christian Dalgaard <jcd@netcompany.com>
2023-12-03 11:13:11 +0100

    Day 3 solution 1

---
+++ 2023/day 3/AOC3/Filereader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC3
+{
+    internal class Filereader
+    {
+        public static List<string> ReadTextFile()
+        {
+            string path = @"C:\source\AOC\2023\day 3\AOC3\example.txt";
+            List<string> lines = new List<string>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    lines.Add(s);   
+                }
+                return lines;
+            }
+
+        }
+    }
+}
---
+++ 2023/day 3/AOC3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AOC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sol s = new Sol(Filereader.ReadTextFile());
+            s.FindAllNumberPositions();
+            s.GetSum();
+        }
+
+    }
+}
