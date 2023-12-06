b68ec87d96ae55324bf1edf334f88a5718065b1c
Jens Christian Dalgaard <jcd@netcompany.com>
2023-12-06 18:46:51 +0100

    Day 5 challenge 2

--- 2023/day 5/src/main/java/org/example/Main.java
+++ 2023/day 5/src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +12,8 @@ public class Main {
 
         List<String> lines =  rf.getLines();
         List<String> seeds = List.of(lines.get(0).replace("seeds: ","").split(" "));
+
+
 
         MapCreator mc = new MapCreator(lines);
 
@@ -20,7 +25,7 @@ public class Main {
         Maps temperatureToHumidity =  mc.createMaps();
         Maps humiditytoLocation =  mc.createMaps();
 
-        long lowestNumber = -1;
+       /* long lowestNumber = -1;
         for (String seed: seeds) {
 
             long start = Long.parseLong(seed);
@@ -36,6 +41,28 @@ public class Main {
 
             }else {
                 lowestNumber = Math.min(lowestNumber,start);
+            }
+        }*/
+        long lowestNumber = -1;
+        for (int i = 0; i < seeds.size(); i = i +2) {
+
+            System.out.println("Round is starting...");
+            for (int j = 0; j < Long.parseLong(seeds.get(i + 1)); j++) {
+
+                long start = Long.parseLong(seeds.get(i)) + j;;
+                start = seedsToSoil.convertNumber(start);
+                start = soilToFertilizer.convertNumber(start);
+                start = fertilizerToWater.convertNumber(start);
+                start = waterToLight.convertNumber(start);
+                start = lightToTemperature.convertNumber(start);
+                start = temperatureToHumidity.convertNumber(start);
+                start = humiditytoLocation.convertNumber(start);
+                if (lowestNumber == -1) {
+                    lowestNumber = start;
+
+                } else {
+                    lowestNumber = Math.min(lowestNumber, start);
+                }
             }
         }
 
