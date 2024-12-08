4f0dbfda93782f87d6dbc39021abc7185a505a8f
Jens Christian Dalgaard <jecd@itu.dk>
2024-12-08 19:24:56 +0100

    a bit of cleanup

--- 2024/day_8/src/main.rs
+++ 2024/day_8/src/main.rs
@@ -6,51 +6,28 @@ fn find_antinodes_for_char(
     coord: &HashSet<(i32, i32)>,
     max_y: i32,
     max_x: i32,
-) -> Result<HashSet<(i32, i32)>, String> {
+) -> Result<(HashSet<(i32, i32)>, HashSet<(i32, i32)>), String> {
     let mut antinodes_for_char = HashSet::<(i32, i32)>::new();
+    let mut antinodes_for_char2 = HashSet::<(i32, i32)>::new();
 
     for x in coord {
         for y in coord {
             if y != x {
+                antinodes_for_char2.insert(*x);
+                antinodes_for_char2.insert(*y);
                 let diff = ((x.0 - y.0), (x.1 - y.1));
-
-                let an1 = (x.0 + diff.0, x.1 + diff.1);
-
-                if an1.0 >= 0 && an1.0 < max_y && an1.1 >= 0 && an1.1 < max_x {
-                    antinodes_for_char.insert(an1.clone());
-                }
-            }
-        }
-    }
-
-    Ok(antinodes_for_char)
-}
-
-fn find_antinodes_for_char_2(
-    coord: &HashSet<(i32, i32)>,
-    max_y: i32,
-    max_x: i32,
-) -> Result<HashSet<(i32, i32)>, String> {
-    let mut antinodes_for_char = HashSet::<(i32, i32)>::new();
-
-    for x in coord {
-        for y in coord {
-            if y != x {
-                antinodes_for_char.insert(*x);
-                antinodes_for_char.insert(*y);
-                let diff = ((x.0 - y.0), (x.1 - y.1));
-
                 let mut an1 = (x.0 + diff.0, x.1 + diff.1);
 
                 if an1.0 >= 0 && an1.0 < max_y && an1.1 >= 0 && an1.1 < max_x {
                     antinodes_for_char.insert(an1.clone());
+                    antinodes_for_char2.insert(an1.clone());
                 }
 
                 let mut check = true;
                 while check {
                     an1 = (an1.0 + diff.0, an1.1 + diff.1);
                     if an1.0 >= 0 && an1.0 < max_y && an1.1 >= 0 && an1.1 < max_x {
-                        antinodes_for_char.insert(an1.clone());
+                        antinodes_for_char2.insert(an1.clone());
                     } else {
                         check = false
                     }
@@ -59,7 +36,7 @@ fn find_antinodes_for_char_2(
         }
     }
 
-    Ok(antinodes_for_char)
+    Ok((antinodes_for_char, antinodes_for_char2))
 }
 
 fn result(matrix: &Vec<Vec<char>>) -> Result<(i32, i32), String> {
@@ -95,16 +72,8 @@ fn result(matrix: &Vec<Vec<char>>) -> Result<(i32, i32), String> {
             find_antinodes_for_char(hm.get(x).expect("Error"), y_len as i32, x_len as i32)
                 .expect("Error");
 
-        let found_coords2 =
-            find_antinodes_for_char_2(hm.get(x).expect("Error"), y_len as i32, x_len as i32)
-                .expect("Error");
-
-        for y in found_coords {
-            antinodes.insert(y);
-        }
-        for y in found_coords2 {
-            antinodes2.insert(y);
-        }
+        antinodes.extend(found_coords.0);
+        antinodes2.extend(found_coords.1);
     }
 
     Ok((antinodes.len() as i32, antinodes2.len() as i32))
